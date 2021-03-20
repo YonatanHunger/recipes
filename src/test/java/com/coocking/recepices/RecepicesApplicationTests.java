f224238e2d6c291fdc899323b7e968b3604e4acf
YonatanHunger <28837109+YonatanHunger@users.noreply.github.com>
2021-03-20 16:58:00 +0200

    Init project

---
+++ src/test/java/com/coocking/recepices/RecepicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coocking.recepices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
